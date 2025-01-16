f1883dbd3b6688a047ade23361e26162288fa221
Arslan Giniiatullin <arslan-gin@yandex-team.ru>
2025-01-16 15:31:18 +0300

    Added client profiles configuration

--- navigator/src/main/kotlin/com/ifmo/se/navigator/configs/ApiConfig.kt
+++ navigator/src/main/kotlin/com/ifmo/se/navigator/configs/ApiConfig.kt
@@ -1,4 +1,24 @@
 package com.ifmo.se.navigator.configs
 
-class ApiConfig {
+import generated.com.ifmo.se.route.api.LocationsApi
+import generated.com.ifmo.se.route.api.RoutesApi
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ApiConfig(
+    @Value("\${client.route-management.server.url}")
+    private val routeManagementUrl: String,
+) {
+
+    @Bean
+    fun locationsApi() = LocationsApi(
+        baseUrl = routeManagementUrl,
+    )
+
+    @Bean
+    fun routesApi() = RoutesApi(
+        baseUrl = routeManagementUrl,
+    )
 }
--- navigator/src/main/kotlin/com/ifmo/se/navigator/services/NavigatorService.kt
+++ navigator/src/main/kotlin/com/ifmo/se/navigator/services/NavigatorService.kt
@@ -1,25 +1,32 @@
 package com.ifmo.se.navigator.services
 
+import com.ifmo.se.navigator.models.Coordinate
 import com.ifmo.se.navigator.models.EnrichedRoute
+import com.ifmo.se.navigator.models.LocationId
 import com.ifmo.se.navigator.models.Route
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Service
-import com.ifmo.se.navigator.models.LocationId
 
 @Service
 class NavigatorService(
     private val locationManagementService: LocationManagementService,
     private val routeManagementService: RouteManagementService,
 ) {
-    internal suspend fun addRoute(idFrom: LocationId, idTo: LocationId, distance: Double, name: String): EnrichedRoute =
+    internal suspend fun addRoute(
+        idFrom: LocationId,
+        idTo: LocationId,
+        distance: Double,
+        name: String,
+        coordinate: Coordinate?,
+    ): EnrichedRoute =
         coroutineScope {
             val locationFromDeferred = async { locationManagementService.getLocationById(locationId = idFrom) }
             val locationToDeferred = async { locationManagementService.getLocationById(locationId = idTo) }
             routeManagementService.addRoute(
                 route = Route(
                     name = name,
-                    coordinate = null,
+                    coordinate = coordinate,
                     locationFrom = locationFromDeferred.await(),
                     locationTo = locationToDeferred.await(),
                     distance = distance,
