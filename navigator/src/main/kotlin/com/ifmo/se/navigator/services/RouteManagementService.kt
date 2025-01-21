22b8f5a786eee1a37e83640f0e2cb69aae822ea4
Arslan Giniiatullin <arslan-gin@yandex-team.ru>
2025-01-21 22:04:03 +0300

    Added getRouteFirstMatched impl

--- navigator/src/main/kotlin/com/ifmo/se/navigator/services/RouteManagementService.kt
+++ navigator/src/main/kotlin/com/ifmo/se/navigator/services/RouteManagementService.kt
@@ -5,6 +5,8 @@ import com.ifmo.se.navigator.mappers.toRouteUpsertRequestDto
 import com.ifmo.se.navigator.models.EnrichedRoute
 import com.ifmo.se.navigator.models.Route
 import generated.com.ifmo.se.route.api.RoutesApi
+import generated.com.ifmo.se.route.dto.GetRoutesFilterParameterDto
+import generated.com.ifmo.se.route.dto.SortFieldsDto
 import mu.KLogging
 import org.springframework.stereotype.Service
 
@@ -20,4 +22,16 @@ class RouteManagementService(
         ).body().toDomain().also {
             logger.info { "Added route=$it" }
         }
+
+    internal suspend fun getRouteFirstMatched(
+        limit: Int?,
+        offset: Int?,
+        sortBy: List<SortFieldsDto>?,
+        filter: GetRoutesFilterParameterDto?,
+    ): EnrichedRoute? =
+        routesApi.getRoutes(filter = filter, sortBy = sortBy, limit = limit, offset = offset)
+            .body().routes?.firstOrNull()
+            ?.toDomain().also {
+                logger.info { "Get route=$it by filter = $filter, sortBy = $sortBy, limit = $limit, offset = $offset" }
+            } // TODO(null or 404?)
 }
